797e198bb0f84543c99770cc3d36da998eda6db7
Denys Madureira <denys.madureira@pm.me>
2025-12-14 17:39:18 +0000

    Feat: Add Simple Icons support with unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          prefix: 'icon',
+          enabledCollections: ['simple-icons'],
+        }),
+      ],
+      dts: true,
+    }),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true,
+    }),
+  ],
 })
