1faf17e8fc5cebd1dda21185b502c743d6a82062
Denys Madureira <denys.madureira@pm.me>
2025-12-14 14:25:52 +0000

    Feat: Add ServiceCard component

---
+++ src/components/molecules/ServiceCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Button from '../atoms/Button.vue'
+import Card from '../atoms/Card.vue'
+
+interface Props {
+  title: string
+  description: string
+  image: string
+  sourceCodeUrl: string
+  installationUrl: string
+  direction?: 'row' | 'column'
+}
+
+withDefaults(defineProps<Props>(), {
+  direction: 'row',
+})
+</script>
+
+<template>
+  <Card :direction="direction">
+    <img class="border-2 border-black" :src="image" :alt="`${title} image`" />
+
+    <div class="flex flex-col gap-4">
+      <h3 class="text-xl font-semibold">{{ title }}</h3>
+
+      <p class="line-clamp-2 text-pretty -mt-2 flex-1">
+        {{ description }}
+      </p>
+
+      <div class="flex gap-4 self-end">
+        <Button variant="underline" :href="sourceCodeUrl">Source Code</Button>
+        <Button :href="installationUrl">Install</Button>
+      </div>
+    </div>
+  </Card>
+</template>
