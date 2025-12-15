afebd8dc4e8d83c70b55f1cb73d9336b9c3649fa
Denys Madureira <denys.madureira@pm.me>
2025-12-15 02:05:10 +0000

    Feat: Add hover and focus styles to Button component
    - Add hover:bg-black and hover:text-white to base classes
    - Add focus:ring-2 focus:ring-neutral-300 to base classes
    - Remove focus:ring-green-300 from underline variant
    - Remove hover:bg-green-300 and focus:ring-green-300 from default
      variant

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -10,14 +10,15 @@ const props = withDefaults(defineProps<Props>(), {
 })
 
 const buttonClasses = computed(() => {
-  const baseClasses = 'px-5 py-3 font-semibold text-black focus:outline-0'
+  const baseClasses =
+    'px-5 py-3 font-semibold text-black focus:outline-0 hover:bg-black hover:text-white focus:ring-2 focus:ring-neutral-300'
 
   if (props.variant === 'underline') {
-    return `${baseClasses} relative border-black bg-white after:absolute after:inset-x-0 after:bottom-0 after:h-1 after:bg-black hover:text-white hover:after:h-full focus:ring-2 focus:ring-green-300 focus:outline-0`
+    return `${baseClasses} relative border-black bg-white after:absolute after:inset-x-0 after:bottom-0 after:h-1 after:bg-black hover:after:h-full focus:outline-0`
   }
 
   // Default variant
-  return `${baseClasses} border-2 border-black bg-white shadow-[4px_4px_0_0] hover:bg-green-300 focus:ring-2 focus:ring-green-300`
+  return `${baseClasses} border-2 border-black bg-white shadow-[4px_4px_0_0]`
 })
 </script>
 
